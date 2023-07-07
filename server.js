5e172ab0ced413b6e7cadb333efeade9abbf0069
shivamxaryan <shivamaryan64@gmail.com>
2023-07-07 13:59:07 +0530

    updating server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-const express=require("express");
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+
+//configure env 
+dotenv.config(); 
+
+
+const PORT = process.env.PORT || 8000;
+
+
+//rest api
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to ecommerce app</h1>");
+  });
+  
+
+  app.listen(PORT, () => {
+    console.log("Server Running on",PORT);
+  });
