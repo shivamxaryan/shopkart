a66d4f7a438814c2ef847c28be0ae4ebca6d1cce
shivamxaryan <shivamaryan64@gmail.com>
2023-07-15 00:13:08 +0530

    creating header and footer layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import Layout from './components/Layout/Layout';
+import {Routes,Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import PagenotFound from './pages/PagenotFound';
 
 function App() {
   return (
     <>
-    <Layout>
-      <h1>Ecommerce App</h1>
-    </Layout>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/policy' element={<Policy />} />
+      <Route path='/*' element={<PagenotFound />} />
+    </Routes>
     </>
   );
 }
